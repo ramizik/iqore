080b9c5d814da73bfce8aead45a4bb2a183048e6
Ramis H. <r_hasanli@u.pacific.edu>
2025-07-09 19:30:52 -0700

    Updated server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = 8000;
+const PORT = 3000;
 
 // MIME types for different file extensions
 const mimeTypes = {
